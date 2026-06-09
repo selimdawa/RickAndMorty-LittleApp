bdf4335cc1b67be05effd21acc44e860a76eb860
selimdawa <selimdawa@gmail.com>
2026-06-09 03:57:36 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,15 +10,13 @@ plugins {
 android {
     namespace = "com.littleapp.rickandmorty"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+       version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.rickandmorty"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
